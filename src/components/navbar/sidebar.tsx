599343442114e01eecfb97d1eaded6f10d8c4f9c
Jean Bayer <jhanbayer@gmail.com>
2025-07-09 22:38:05 -0400

    feat: admin badge en sidebar

--- src/components/navbar/sidebar.tsx
+++ src/components/navbar/sidebar.tsx
@@ -12,6 +12,9 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar";
 import { useUser } from "@/hooks/use-user";
+import { ShieldUser } from "lucide-react";
+import { AdminConditional } from "../membership/admin-conditional";
+import { Badge } from "../ui/badge";
 
 export const AppSidebar = () => {
   const userData = useUser();
@@ -19,7 +22,18 @@ export const AppSidebar = () => {
   return (
     <Sidebar>
       <SidebarHeader>
-        <h3 className="text-center p-2">{userData.data?.name}</h3>
+        <div className="flex items-center">
+          <h3 className="flex-1 text-center p-2">{userData.data?.name}</h3>
+          <AdminConditional>
+            <Badge
+              variant="secondary"
+              className="bg-blue-500 text-white dark:bg-blue-600"
+            >
+              <ShieldUser size={16} />
+              Admin
+            </Badge>
+          </AdminConditional>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         <HomeMenu />
