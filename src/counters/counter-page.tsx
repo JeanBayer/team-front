f5e5cf60bd7b7734df5d5d1447a7401c696730a7
Jean Bayer <jhanbayer@gmail.com>
2025-07-16 13:49:48 -0400

    feat: añado mejoras de ui en la card del counter en el listado de counters

--- src/counters/counter-page.tsx
+++ src/counters/counter-page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/header/header";
 import { CustomTooltip } from "@/components/tooltip/custom-tooltip";
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useCounter } from "@/counters/hooks/use-counter";
 import { useTeam } from "@/teams/hooks/use-team";
@@ -38,29 +39,53 @@ export const CounterPage = () => {
       />
 
       <section className="flex gap-8 flex-wrap justify-center p-8 max-w-2xl mx-auto">
-        {counters.data?.map((counter) => (
-          <Link
-            to={`${counter.id}`}
-            className="inline-block cursor-pointer"
-            key={counter.id}
-          >
-            <Card className="w-44 h-44 hover:shadow-lg hover:border-blue-300 transition-all duration-300 gap-4">
-              <CardHeader className="flex-1">
-                <CustomTooltip label={counter.name}>
-                  <CardTitle className="text-center text-lg font-semibold text-blue-500 line-clamp-3">
-                    {counter.name}
-                  </CardTitle>
-                </CustomTooltip>
-              </CardHeader>
-              <CardContent>
-                <p className="text-center">
-                  <strong>{counter.currentCount}</strong>
-                </p>
-              </CardContent>
-            </Card>
-          </Link>
-        ))}
+        {counters.data?.map(
+          ({ id, name, alreadyModifiedToday, currentCount }) => (
+            <Link to={`${id}`} className="inline-block cursor-pointer" key={id}>
+              <Card className="w-40 h-40 hover:shadow-lg hover:border-blue-300 transition-all duration-300 gap-4 relative p-0">
+                {!alreadyModifiedToday && (
+                  <Badge
+                    variant="destructive"
+                    className="text-[8px] absolute top-1 -right-5"
+                  >
+                    Pendiente
+                  </Badge>
+                )}
+                <div className="relative w-full h-full overflow-hidden p-4 flex flex-col">
+                  <CardHeader className="flex-1 p-0">
+                    <CustomTooltip label={name}>
+                      <CardTitle className="text-center text-sm font-semibold text-blue-500 line-clamp-3">
+                        {name}
+                      </CardTitle>
+                    </CustomTooltip>
+                  </CardHeader>
+                  <CardContent className="p-0">
+                    <p className="text-center text-2xl">
+                      <strong>{currentCount}</strong>
+                    </p>
+                  </CardContent>
+                  {!alreadyModifiedToday && (
+                    <img
+                      className="absolute w-16 -bottom-5 -right-5 z-0 rotate-[23deg]"
+                      src="/assets/sand-clock.png"
+                      alt="sand-clock"
+                    />
+                  )}
+                </div>
+              </Card>
+            </Link>
+          )
+        )}
       </section>
     </div>
   );
 };
+
+// width: 66px;
+// /* top: -8px; */
+// /* right: -8px; */
+// bottom: 0;
+// right: 0;
+// rotate: 23deg;
+// overflow: hidden;
+// z-index: 0;
