aeb63aa4f260954e777b8a057012d703670e1251
Jean Bayer <jhanbayer@gmail.com>
2025-07-21 22:29:32 -0400

    ui: actualiza el estilo del GoalLoader con nuevos estilos de fondo y borde

--- src/goals/components/loaders/goal-loader.tsx
+++ src/goals/components/loaders/goal-loader.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export const GoalLoader = () => {
   return (
-    <div className="flex mb-2 items-center justify-between bg-blue-100 p-4 rounded-b-lg">
+    <div className="flex mb-2 items-center justify-between bg-card border-2 p-4 rounded-b-lg">
       <div>
         <Skeleton className="h-4 w-36 mb-2" />
         <Skeleton className="h-4 w-46" />
